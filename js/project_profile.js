ee205be8138e755432427de0916896b490ebc710
JeyWhy <julia.yang@mail.utoronto.ca>
2017-02-17 14:22:42 -0500

    fixed the html and css to make the page look better, made the default page opened to About tab in large screens

--- js/project_profile.js
+++ js/project_profile.js
@@ -1,23 +1,4 @@
-/*$(document).ready(function() {
-    // run test on initial page load
-    checkSize();
-
-    // run test on resize of the window
-    $(window).resize(checkSize);
-});
-
-//Function to the css rule
-function checkSize(){
-    if ($(".checkSize").css("float") == "none" ){
-        expandTab(tabName);
-    }
-	else if ($(".checkSize").css("float") == "left" ){
-        openTab(tabName);
-    }
-}*/
-
-$(window).on("resize", emptySelection);
-
+// When the page is first loaded & when the window is resized, erase the opened tabs
 function emptySelection(){
 	var i;
 	var x = document.getElementsByClassName("expand_tab");
@@ -29,12 +10,17 @@ function emptySelection(){
 	for (i = 0; i < x.length; i++) {
 	   x[i].style.display = "none";  
 	}
+	// if the screen is larger than 768px, display the "About" tab by default
+	var mediaQuery = window.matchMedia("screen and (min-width: 768px)");
+	if(mediaQuery.matches){
+		document.getElementById('About_big').style.display = "block";
+	}
 }
 
 function expandTab(tabName) {
 	var i;
-	var mql = window.matchMedia("screen and (min-width: 768px)");
-	if (mql.matches){ // if media query matches
+	var mediaQuery = window.matchMedia("screen and (min-width: 768px)");
+	if (mediaQuery.matches){ // if media query matches
 		var idName = tabName + "_big";
 		var x = document.getElementsByClassName("expand_tab");
 		for (i = 0; i < x.length; i++) {
@@ -60,26 +46,3 @@ function expandTab(tabName) {
 		document.getElementById(tabName).style.display = "block";  
 	}
 }
-/*
-if($(window).width() <= 480){
-	function expandTab(tabName) {
-		var i;
-		var x = document.getElementsByClassName("expand_tab");
-		for (i = 0; i < x.length; i++) {
-		   x[i].style.display = "none";  
-		}
-		document.getElementById(tabName).style.display = "block";  
-	}
-}
-else{
-	function expandTab(tabName) {
-		var i;
-		var idName = tabName + "_big";
-		var x = document.getElementsByClassName("tab");
-		for (i = 0; i < x.length; i++) {
-		   x[i].style.display = "none";  
-		}
-		document.getElementById(idName).style.display = "block";  
-	}
-}
-*/
