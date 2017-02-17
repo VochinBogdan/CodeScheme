9d1d8a036104a295b33b838ba6022906de2f23f9
JeyWhy <julia.yang@mail.utoronto.ca>
2017-02-17 14:50:21 -0500

    fixed errors from validators

--- js/project_profile.js
+++ js/project_profile.js
@@ -1,48 +1,47 @@
 // When the page is first loaded & when the window is resized, erase the opened tabs
-function emptySelection(){
-	var i;
-	var x = document.getElementsByClassName("expand_tab");
-	for (i = 0; i < x.length; i++) {
-	   x[i].style.display = "none";  
-	}
-	
-	x = document.getElementsByClassName("tab");
-	for (i = 0; i < x.length; i++) {
-	   x[i].style.display = "none";  
-	}
-	// if the screen is larger than 768px, display the "About" tab by default
-	var mediaQuery = window.matchMedia("screen and (min-width: 768px)");
-	if(mediaQuery.matches){
-		document.getElementById('About_big').style.display = "block";
-	}
+function emptySelection() {
+    var i;
+    var x = document.getElementsByClassName("expand_tab");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+
+    x = document.getElementsByClassName("tab");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    // if the screen is larger than 768px, display the "About" tab by default
+    var mediaQuery = window.matchMedia("screen and (min-width: 768px)");
+    if (mediaQuery.matches) {
+        document.getElementById('About_big').style.display = "block";
+    }
 }
 
 function expandTab(tabName) {
-	var i;
-	var mediaQuery = window.matchMedia("screen and (min-width: 768px)");
-	if (mediaQuery.matches){ // if media query matches
-		var idName = tabName + "_big";
-		var x = document.getElementsByClassName("expand_tab");
-		for (i = 0; i < x.length; i++) {
-		   x[i].style.display = "none";  
-		}
-		
-		x = document.getElementsByClassName("tab");
-		for (i = 0; i < x.length; i++) {
-		   x[i].style.display = "none";  
-		}
-		document.getElementById(idName).style.display = "block";
-	}
-	else{
-		var x = document.getElementsByClassName("tab");
-		for (i = 0; i < x.length; i++) {
-		   x[i].style.display = "none";  
-		}
-		
-		x = document.getElementsByClassName("expand_tab");
-		for (i = 0; i < x.length; i++) {
-		   x[i].style.display = "none";  
-		}
-		document.getElementById(tabName).style.display = "block";  
-	}
+    var i;
+    var mediaQuery = window.matchMedia("screen and (min-width: 768px)");
+    if (mediaQuery.matches) { // if media query matches
+        var idName = tabName + "_big";
+        var x = document.getElementsByClassName("expand_tab");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+
+        x = document.getElementsByClassName("tab");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        document.getElementById(idName).style.display = "block";
+    } else {
+        var x = document.getElementsByClassName("tab");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+
+        x = document.getElementsByClassName("expand_tab");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        document.getElementById(tabName).style.display = "block";
+    }
 }
