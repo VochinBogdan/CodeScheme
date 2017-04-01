35cc1757bf4bd02addf933c25e6d3ca67cd0d0f6
JeyWhy <julia.yang@mail.utoronto.ca>
2017-03-31 21:42:22 -0400

    processed skills input as string before sending

--- js/projectEdit.js
+++ js/projectEdit.js
@@ -26,3 +26,33 @@ function addSkills()
 		document.getElementById("add_button").style.display = "none";
 	}
 }
+
+// processes the skills input
+function processSkills()
+{
+	var skill1, skill1_val, skill2, skill2_val, skill3, skill3_val;
+	var skill_input;
+	
+	// skill 1
+	skill1_val = document.getElementById("range1").value;
+	document.getElementById("result1").innerHTML=skill1_val;
+	skill1 = document.getElementById("skill_name1").value;
+	skill_input = "\"skills_used\": [{ \"name\": \"" + skill1 + "\", \"level\": " + skill1_val + "}";
+	
+	if(numSkill > 1){
+		// skill 2
+		skill2_val = document.getElementById("range2").value;
+		document.getElementById("result2").innerHTML=skill2_val;
+		skill2 = document.getElementById("skill_name2").value;
+		skill_input += ",{ \"name\": \"" + skill2 + "\", \"level\": " + skill2_val + "}";
+	}
+	if(numSkill > 2){
+		// skill 3
+		skill3_val = document.getElementById("range3").value;
+		document.getElementById("result3").innerHTML=skill3_val;
+		skill3 = document.getElementById("skill_name3").value;
+		skill_input += ",{ \"name\": \"" + skill3 + "\", \"level\": " + skill3_val + "}";
+	}
+	skill_input += "]";
+	document.getElementById("final_skills").innerHTML=skill_input;
+}
