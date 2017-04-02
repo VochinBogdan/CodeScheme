274c9555e6c77f14095af7ea1ddf4800ca60970c
JustCourt <courtney.j.siu@gmail.com>
2017-04-02 18:50:34 -0400

    Deleted ejs file, added filtering through school and city, let user add skill filters (but not use them)

---
+++ js/projectSearch.js
@@ -0,0 +1,45 @@
+var projectSearchApp = angular.module("projectSearchApp", []);
+
+projectSearchApp.controller('projectSearchController', function($scope, $http) {
+
+    $scope.filteredSkills = [];
+
+    $scope.addFilterSkill = function(newSkill) {
+        if (newSkill){
+            $scope.filteredSkills.push(newSkill);
+            $scope.newSkill = "";
+        }
+    }
+
+    $scope.removeSkill = function(skill) {
+        for (var i=0; i<$scope.filteredSkills.length; i++) {
+            if ($scope.filteredSkills[i] == skill) {
+                $scope.filteredSkills.splice(i, 1);
+            }
+        }
+    }
+    
+    $scope.filterProjects = function(filter) {
+        var url = "/projects?";
+        var params = [];
+        if (filter.city) {
+            params.push("city=" + filter.city);
+        }
+        if (filter.school) {
+            params.push("school=" + filter.school);
+        }
+        url += params.join("&");
+        console.log(url);
+        $http.get(url).then(function ( response ) {
+            recieved_projects = response.data;
+            // Add attribute to projects for current number of members
+            recieved_projects.forEach( function(project) {
+                project.num_members_found = project.contributors.length;
+            }) 
+            
+            $scope.projects = recieved_projects;
+        });
+    }
+
+    $scope.filterProjects([]);
+});
